db49aa3b18641214536fe575b121cbde0f2db06c
Chirag Saraswat <csaraswat87@gmail.com>
2024-12-28 11:16:59 +0530

    swagger assets added for hosting assets locally

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,24 +2,42 @@ import sentry_sdk
 from fastapi import FastAPI
 from fastapi.routing import APIRoute
 from starlette.middleware.cors import CORSMiddleware
+from fastapi.openapi.docs import (
+    get_swagger_ui_html,
+    get_swagger_ui_oauth2_redirect_html
+)
+from fastapi.staticfiles import StaticFiles
 
 from app.api.main import api_router
 from app.core.config import settings
 
 
-def custom_generate_unique_id(route: APIRoute) -> str:
-    return f"{route.tags[0]}-{route.name}"
 
 
 if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
     sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
 
 app = FastAPI(
-    title=settings.PROJECT_NAME,
+    docs_url=None,
+    redoc_url=None,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
-    generate_unique_id_function=custom_generate_unique_id,
 )
 
+app.mount("/static",StaticFiles(directory="/app/app/static"),name="static")
+
+@app.get("/docs",include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=f"{settings.API_V1_STR}/openapi.json",
+        title=settings.PROJECT_NAME,
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="/static/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger-ui.css"
+    )
+
+@app.get(app.swagger_ui_oauth2_redirect_url,include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirectD_html()
 # Set all CORS enabled origins
 if settings.all_cors_origins:
     app.add_middleware(
